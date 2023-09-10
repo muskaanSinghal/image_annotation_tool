0a7815d970a84658eb0bfcecaa55e0dcf925b690
muskaanSinghal <87534297+muskaanSinghal@users.noreply.github.com>
2023-09-10 14:19:10 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#331D2C",
+        secondary: "#3F2E3E",
+        offwhite: "#EFE1D1",
+        accent:'#010101'
+      },
+    },
+  },
+  plugins: [],
+};
